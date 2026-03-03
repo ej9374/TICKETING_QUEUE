cec09ee5d42092432225537eb0bdb4ff49d4de8b
ej9374 <109258160+ej9374@users.noreply.github.com>
2026-03-04 00:07:01 +0900

    feat: Repository 생성

---
+++ src/main/java/eunji/ticketing/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package eunji.ticketing.repository;
+
+import eunji.ticketing.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
---
+++ src/main/java/eunji/ticketing/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package eunji.ticketing.repository;
+
+import eunji.ticketing.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
